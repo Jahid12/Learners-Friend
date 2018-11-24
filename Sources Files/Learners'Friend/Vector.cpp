ebc11e07c3886598842097f70ff96e1c1cfdf288
Jahid12 <45305385+Jahid12@users.noreply.github.com>
2018-11-24 18:07:01 +0600

    Vector.cpp
    Addition ta thik ase ??

--- Sources Files/Learners'Friend/Vector.cpp
+++ Sources Files/Learners'Friend/Vector.cpp
@@ -36,16 +36,33 @@ void Vector::Basic_idea_about_Vector()
 }
 void Vector::get_vector()
 {
-    cout << "Input: \nVector1:";
-
-    cin >> x1 >> y1 >> z1;
-    cout << "Vector2:";
-    cin >> x2 >> y2 >> z2;
+    cout << "Put the value A1, A2, A3 of the first vector : " ;
+    cin >> x1 >> x2 >> x3;
+    cout << "\nPut the value B1, B2, B3 of the second vector :";
+    cin >> y1 >> y2 >> y3;
 
 }
 void Vector::vector_additon()
 {
     get_vector();
+    int z1,z2,z3;
+    z1=x1+y1;
+    z2=x2+y2;
+    z3=x3+y3;
+    cout << "\nAddition of the two vector is : ";
+    cout << z1 << "i";
+    if(z2<0){
+        cout << z2 << "j";
+    }
+    else{
+        cout << "+" << z2 << "j";
+    }
+    if(z3<0){
+        cout << z3 << "k";
+    }
+    else{
+        cout << "+" << z3 << "k";
+    }
 }
 void Vector::vector_subtraction()
 {
