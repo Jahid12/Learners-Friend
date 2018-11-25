3fa901b2c8f5e7de4d6067b654cf21fa5c83a689
Jahid12 <45305385+Jahid12@users.noreply.github.com>
2018-11-25 09:49:55 +0600

    Update Kinematics.cpp
    Constant velocity

--- Sources Files/Learners'Friend/Kinematics.cpp
+++ Sources Files/Learners'Friend/Kinematics.cpp
@@ -18,7 +18,13 @@ void Kinematics::basic_theories_of_kinematics()
 }
 void Kinematics::constant_velocity()
 {
-    //under working
+    int v,t,s;
+    cout << "At uniform velocity distant s=vt.where v is the uniform velocity,t is time.\n" << "For finding distant put the value of v=";
+    cin >> v;
+    cout << "\t\t\t\t t=";
+    cin >> t;
+    s=v*t;
+    cout << "Distance is : " << s;
 }
 void Kinematics::acceleration()
 {
